ca8c39cda6f41b460743c865db43f2b5fc3e35d5
Melissa McFadzien <92661031+pickles-and-olives@users.noreply.github.com>
2021-12-21 19:28:35 +1300

    Add files via upload
    Lesson 2 - Mad Libs Exercise

---
+++ Lesson 2 Activity/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+{
+string adjective1, verb, pronoun, adjective2, adjective3;
+
+Console.Write("Enter an adjective: ");
+adjective1 = Console.ReadLine();
+
+Console.Write("Enter a verb: ");
+verb = Console.ReadLine();
+
+Console.Write("Enter a pronoun: ");
+pronoun = Console.ReadLine();
+
+Console.Write("Enter another adjective: ");
+adjective2 = Console.ReadLine();
+
+Console.Write("Enter one more adjective: ");
+adjective3 = Console.ReadLine();
+
+Console.WriteLine($"My cat is a very " + adjective1 + " creature.");
+Console.WriteLine($"She loves to " + verb + "all day, with no regard to " + pronoun + ".");
+Console.WriteLine($"But, even when she is " + adjective2 + ", she is still totally " + adjective3 + ".");
+
+Console.ReadLine();
+
+}
