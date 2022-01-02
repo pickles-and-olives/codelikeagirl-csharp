6be4d6517331a3f48feae5d95b05e6f6ded83401
Melissa McFadzien <92661031+pickles-and-olives@users.noreply.github.com>
2022-01-02 19:33:36 +1300

    Add files via upload
    Not confident about this exercise, would like to improve and negate the potential problem areas that VS code idendified.

---
+++ Lesson 3 Activity/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+{
+Console.Write("Initial deposit: ");
+var deposit = int.Parse(Console.ReadLine());
+
+
+Console.Write("Monthly interest rate (as percentage): ");
+var interestAsPercentage = double.Parse(Console.ReadLine());
+
+Console.Write("Number of Months: ");
+var months = int.Parse(Console.ReadLine());
+
+
+var savings = deposit * Math.Pow(1 + (interestAsPercentage/100),months);
+Console.WriteLine($"Total Savings: " + savings); 
+Console.WriteLine();
+
+}
