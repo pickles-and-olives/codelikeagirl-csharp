f753c8469bf5b70956826b07bddedc059d53138f
Melissa McFadzien <92661031+pickles-and-olives@users.noreply.github.com>
2022-01-08 21:19:09 +1300

    Add files via upload

---
+++ Lesson 4 Class Code-Along/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Out of the following, what sort of movie do you feel like watching? \nFunny, Scary, Thrilling, Inspiring");
+var typeOfFilm = Console.ReadLine();
+
+string filmTitle;
+
+if(typeOfFilm == "Funny" || typeOfFilm == "funny" || typeOfFilm == "FUNNY")
+{
+    filmTitle = "Booksmart";
+}
+
+else if(typeOfFilm == "Scary" || typeOfFilm == "scary" || typeOfFilm == "SCARY")
+{
+    filmTitle = "Get Out";
+}
+
+else if(typeOfFilm == "Thrilling" || typeOfFilm == "thrilling" || typeOfFilm == "THRILLING")
+{
+    filmTitle = "No Time To Die";
+}
+
+else if(typeOfFilm == "Inspiring" || typeOfFilm == "inspiring" || typeOfFilm == "INSPIRING")
+{
+    filmTitle = "The Intouchables";
+}
+
+else 
+{
+    filmTitle = "Harry Potter";
+}
+
+Console.WriteLine($"I reccomend you watch {filmTitle}.");
