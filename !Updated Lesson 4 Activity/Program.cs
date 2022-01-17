afcc3487b86dea8ff595d3c897a5557873dd3903
Melissa McFadzien <92661031+pickles-and-olives@users.noreply.github.com>
2022-01-17 18:39:05 +1300

    Add files via upload
    Further work on activity #4, after discussing with cohort. Succeeded in adding hints (high/low) and guess count, but not sure how to limit guesses in this format.

---
+++ !Updated Lesson 4 Activity/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var randomGenerator = new Random();
+int i = randomGenerator.Next(1,11); //Generated number
+int j = 0; //User input
+int guessCount = 0;
+
+while(guessCount <= 3)
+{
+while(j != i)
+{
+    guessCount++;
+
+    Console.WriteLine("Guess a number from 1 to 10: ");
+    int.TryParse(Console.ReadLine(), out j);
+
+    if(j > i)
+    {
+        Console.WriteLine("Your guess was too high.");
+        Console.WriteLine($"You have guessed {guessCount} times");
+    }
+    else if(j < i)
+    {
+        Console.WriteLine("Your guess was too low.");
+        Console.WriteLine($"You have guessed {guessCount} times");
+    }
+    else
+    {
+        Console.WriteLine("You guessed correctly!");
+    }
+}
+}
