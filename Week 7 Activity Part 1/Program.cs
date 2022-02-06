2cbdcfc4181bbd5b98e1de5152b9ca645d98b0a8
Melissa McFadzien <92661031+pickles-and-olives@users.noreply.github.com>
2022-02-06 15:50:30 +1300

    Add files via upload
    Task one of the activity for week 7, unsure if task two needs to work alongside task one, so uploading here before continuing.

---
+++ Week 7 Activity Part 1/Program.cs
@@ -0,0 +1,46 @@
+﻿
+var member1 = new Member
+{
+    firstName = "Leslie",
+    lastName = "Phillips",
+    favouriteBook = "A Little Life",
+};
+
+var member2 = new Member
+{
+    firstName = "Bianca",
+    lastName = "Nolan",
+    favouriteBook = "Normal People",
+};
+
+var member3 = new Member
+{
+    firstName = "Emma",
+    lastName = "Brown",
+    favouriteBook = "The Goldfinch",
+};
+
+var members = new List<Member>
+{
+member1, member2, member3
+};
+
+foreach(var member in members)
+{
+member.displayFavouriteBook();
+}
+public class Member
+{
+    public string? firstName;
+    public string? lastName;
+    public string? favouriteBook;
+
+    public void displayFavouriteBook()
+    {
+        Console.WriteLine($"{firstName} {lastName}'s favourite book is {favouriteBook}.");
+    }
+}
+
+
+
+
