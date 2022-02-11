637eb80e18e29fdc54ba481ce8c48c6b569ef093
Melissa McFadzien <92661031+pickles-and-olives@users.noreply.github.com>
2022-02-11 17:13:12 +1300

    Update Program.cs
    Got the program working successfully! Trying to find the best way to add an escape key component now.

--- Week 6/Program.cs
+++ Week 6/Program.cs
@@ -1,81 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using System;
-using System.Collections.Generic;
+//using System.Collections.Generic;
 
 Console.WriteLine("Welcome to Melissa's basic calculator program.");
 
 //var validOperators = new List<string>{"+", "-", "/", "*"};
-int firstNumber = 0;
-int secondNumber = 0;
-string mathCommand = " ";
+int firstNumber;
+int secondNumber;
+char mathCommand;
+//var exitCommand = "x";
 
-while(true)
-{
-
-    getFirstNumber(firstNumber);
-
-    getSecondNumber(secondNumber);
-
-    //getMathCommand(mathCommand);
-
-    calculateSum(firstNumber, secondNumber, mathCommand);
-
-    continueEscapeKey();
-    
-    break;
-}
-
-    static void getFirstNumber(int firstNumber)
     {
-    Console.WriteLine("Please enter the first number in your equation: ");
-    int.TryParse(Console.ReadLine(), out firstNumber);
 
-    }
-    static void getSecondNumber(int secondNumber)
-    {
-    Console.WriteLine("Please enter the second number in your equation: ");
-    int.TryParse(Console.ReadLine(), out secondNumber);
+        firstNumber = getFirstNumber();
+
+        secondNumber = getSecondNumber();
+
+        mathCommand = getMathCommand();
+
+        calculateSum(firstNumber, secondNumber, mathCommand);
+        
     }
 
-    // static void getMathCommand(string mathCommand)
+        static int getFirstNumber()
+        {
+        Console.WriteLine("Please enter the first number in your equation: ");
+        int.TryParse(Console.ReadLine(), out int firstNumber);
+        return firstNumber;
+        }
+        static int getSecondNumber()
+        {
+        Console.WriteLine("Please enter the second number in your equation: ");
+        int.TryParse(Console.ReadLine(), out int secondNumber);
+        return secondNumber;
+        }
+
+        static char getMathCommand()
+        {
+        Console.WriteLine("Please enter the operator you would like to use (+, -, /, *)");
+        char.TryParse(Console.ReadLine(), out var mathCommand);
+        return mathCommand;
+        //mathCommand = Console.ReadLine()!;
+        }
+
+        // static void exitKey(string exit)
+        // {
+        // Console.WriteLine("Type x if you wish to exit the program.");
+        // exit = Console.ReadLine()?.ToLower();
+        // while (exit == "x") 
+        // {
+        //     break;
+        // }
+        // }
+
+    static void calculateSum(int firstNumber, int secondNumber, char mathCommand)
+    {
+
+    // while(mathCommand == "+" || mathCommand == "-" || mathCommand == "*" || mathCommand == "/")
     // {
-    // Console.WriteLine("Please enter the operator you would like to use (+, -, /, *)");
-    // mathCommand = Console.ReadLine()!;
-    // }
 
-static void calculateSum(int firstNumber, int secondNumber, string mathCommand)
-{
+        switch(mathCommand)
+        {
+            case '+':
+                Console.WriteLine($"Your result: {firstNumber} + {secondNumber} = " + (firstNumber + secondNumber));
+                break;
 
-Console.WriteLine("Please enter the operator you would like to use (+, -, /, *)");
-mathCommand = Console.ReadLine()!;
+            case '-':
+                Console.WriteLine($"Your result: {firstNumber} - {secondNumber} = " + (firstNumber - secondNumber));
+                break;
 
-while(mathCommand == "+" || mathCommand == "-" || mathCommand == "*" || mathCommand == "/")
-{
-    switch(mathCommand)
-    {
-        case "+":
-            Console.WriteLine($"Your result: {firstNumber} + {secondNumber} = " + (firstNumber + secondNumber));
-            break;
-        case "-":
-            Console.WriteLine($"Your result: {firstNumber} - {secondNumber} = " + (firstNumber - secondNumber));
-            break;
-        case "*":
-            Console.WriteLine($"Your result: {firstNumber} * {secondNumber} = " + (firstNumber * secondNumber));
-            break;
-        case "/":
-            Console.WriteLine($"Your result: {firstNumber} / {secondNumber} = " + (firstNumber / secondNumber));
-            break;
+            case '*':
+                Console.WriteLine($"Your result: {firstNumber} * {secondNumber} = " + (firstNumber * secondNumber));
+                break;
 
-        default:
-        Console.WriteLine("Sorry, please make sure you are entering one of the following: +, -, *, /");
-        break;
-    }
-    break;
-    }
-}
-static void continueEscapeKey()
-{
-Console.WriteLine("Press 'Enter' or 'Return' to continue, or any other key to quit.");
-Console.ReadKey();
-while (Console.ReadKey().Key != ConsoleKey.Enter) {}
-}
+            case '/':
+                Console.WriteLine($"Your result: {firstNumber} / {secondNumber} = " + (firstNumber / secondNumber));
+                break;
+
+                default:
+                Console.WriteLine("Sorry, please make sure you are entering one of the following: +, -, *, /.");
+                break;
+        }
+   }
+
