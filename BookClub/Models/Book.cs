c64a9e51f01d07fb9c44fc43f20b887c822d6dd5
Melissa McFadzien <92661031+pickles-and-olives@users.noreply.github.com>
2022-02-17 10:12:50 +1300

    Add files via upload
    Improved program for Week 7 activity Book Club, using classes and allowing for changes to reading lists. No user input function yet.

---
+++ BookClub/Models/Book.cs
@@ -0,0 +1,8 @@
+namespace BookClub.Models;
+
+public class Book
+{
+    public string? title;
+    public string? author;
+
+}
---
+++ BookClub/Program.cs
@@ -0,0 +1,73 @@
+﻿
+using BookClub.Models;
+
+var book1 =new Book
+{
+title = "All the light we cannot see",
+author = "Anthony Doer"
+};
+
+var book2 =new Book
+{
+title = "The Talented Mr Ripley",
+author = "Patricia Highsmith"
+};
+
+var book3 =new Book{
+    title = "Swimming Lessons",
+    author = "Claire Fuller"
+};
+
+var book4 =new Book{
+    title = "The Great Gatsby",
+    author = "F. Scott Fitzgerald"
+};
+
+var book5 =new Book{
+    title = "Pachinko",
+    author = "Min Jin Lee"
+};
+
+var book6 =new Book
+{
+    title = "White Teeth",
+    author = "Zadie Smith"
+};
+
+var bookList = new List<Book> {book1, book2, book3, book4, book5, book6};
+
+var member1 = new Member();
+member1.firstName = "Leslie";
+member1.lastName = "Phillip";
+member1.favouriteBook = "A Little Life";
+member1.readingList = new List<Book>{bookList[1], bookList[2]};
+
+
+var member2 = new Member();
+member2.firstName = "Bianca";
+member2.lastName = "Nolan";
+member2.favouriteBook = "Normal People";
+member2.readingList = new List<Book>{bookList[3], bookList[4]};
+
+
+var member3 = new Member();
+member3.firstName = "Emma";
+member3.lastName = "Brown";
+member3.favouriteBook = "The Goldfinch";
+member3.readingList = new List<Book>{book5, book6};
+
+
+var members = new List<Member>
+{
+member1, member2, member3
+};
+
+foreach(var member in members)
+{
+member.displayFavouriteBook();
+Console.WriteLine("They would like to read:");
+foreach (var book in member.readingList!)
+        {
+            Console.WriteLine($"{book.title} by {book.author}");
+        }
+}
